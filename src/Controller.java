3c3bb0d12509da5d7deb9fcf6184eb3efdfd29ec
amir <shayeghia@gmail.com>
2018-08-24 01:04:17 -0400

    Initial version for Count III / before JavaFX ADD Application.css :     Controller.java Action listener in JavaFx     Count.Html : We can have a web application???     main.fxml

---
+++ src/Controller.java
@@ -0,0 +1,14 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    public void handleCommand (ActionEvent event){
+        doLoadSession();
+    }
+
+    private void doLoadSession()
+    {
+        throw new UnsupportedOperationException();
+    }
+}
